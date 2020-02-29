f79d2599d8176a216a0e90c27001cc7d835d6cf5
Hendy <hendy.almeida@hotmail.com>
2020-02-29 10:05:22 -0300

    adicionando imagens e conteudo dinamico na home e lista de produtos

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Produto;
 
 class ProdutoController extends Controller
 {
     public function index()
     {
-        return view('produtos');
+        $produtos = Produto::all();
+
+        return view('produtos', compact('produtos'));
     }
 
     public function show()
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -65,51 +65,28 @@
     <section id="produtos" class="new-products mt-5">
         <div class="container text-center">
             <h1>New Products</h1>
-            @if(isset($produtos))
 
-            @foreach($produtos as $produto)
-            {{ $produto['nome'] }} - R$ {{ $produto['preco'] }} <br>
-            @endforeach
-
-            @endif
             <div class="row">
+                @if(isset($produtos))
+
+                @foreach($produtos as $produto)
+
                 <div class="livro col-sm-12 col-lg-4 my-4">
                     <div class="card">
-                        <img class="card-img-top" src="Images/duumy.png" alt="Card image cap">
+                        <img class="card-img-top" src="{{ $produto['imagem'] }}" alt="Card image cap">
                         <div class="card-body">
-                            <h5 class="card-title text-center">Livro 1</h5>
+                            <h5 class="card-title text-center">{{ $produto['nome'] }}</h5>
                             <ul class="list-group list-group-flush text-center">
-                                <li class="list-group-item"><strong>R$ 45,00</strong></li>
+                                <li class="list-group-item"><strong>R$ {{ $produto['preco'] }}</strong></li>
                             </ul>
                             <a href="#" class="d-flex btn btn-primary my-3 justify-content-center comprar">Comprar</a>
                         </div>
                     </div>
                 </div>
-                <div class="livro col-sm-12 col-lg-4 my-4">
-                    <div class="card">
-                        <img class="card-img-top" src="Images/duumy.png" alt="Card image cap">
-                        <div class="card-body">
-                            <h5 class="card-title text-center">Livro 2</h5>
-                            <ul class="list-group list-group-flush text-center">
-                                <li class="list-group-item"><strong>R$ 35,00</strong></li>
-                            </ul>
-                            <a href="#" class="d-flex btn btn-primary my-3 justify-content-center comprar">Comprar</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="livro col-sm-12 col-lg-4 my-4">
-                    <div class="card">
-                        <!-- https://www.youtube.com/watch?v=vFzUVINwnFo pra ver em casa -->
-                        <img class="card-img-top" src="Images/duumy.png" alt="Card image cap">
-                        <div class="card-body">
-                            <h5 class="card-title text-center">Livro 3</h5>
-                            <ul class="list-group list-group-flush text-center">
-                                <li class="list-group-item"><strong>R$ $80,00</strong></li>
-                            </ul>
-                            <a href="#" class="d-flex btn btn-primary my-3 justify-content-center comprar">Comprar</a>
-                        </div>
-                    </div>
-                </div>
+                @endforeach
+
+                @endif
+
             </div>
         </div>
     </section>
